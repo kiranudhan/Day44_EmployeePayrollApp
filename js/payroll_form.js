ed0c44f1806aedfac8c7d5c8405f9937f65e6c46
kiranudhan <udhankiran62@gmail.com>
2023-01-29 09:29:52 +0530

    UC3 ->on save create Employee payroll Object

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -28,3 +28,81 @@ window.addEventListener( 'DOMContentLoaded', (event) => {
 	   output.textContent = salary.value;
 	});
 });
+
+
+
+const save = () =>
+{
+	try
+	{
+		let employeePayrollData = createEmployeePayroll();
+		createAndUpdateStrorage(employeePayrollData);
+	}
+	catch (e)
+	{
+		console.log(e);
+		return;
+	}
+}
+
+function createAndUpdateStrorage(employeePayrollData)
+{
+	let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+
+	if(employeePayrollList != undefined)
+	{
+		employeePayrollList.push(employeePayrollData);
+	}
+	else
+	{
+		employeePayrollList = [employeePayrollData];
+	}
+	alert(employeePayrollList.toString());
+	localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList));
+}
+
+const createEmployeePayroll = () =>
+{
+	let employeePayrollData = new EmployeePayrollData();
+	try
+	{
+		employeePayrollData.name = getInputValueById('#name');
+	}
+	catch (e)
+	{
+		setTextValue('.text-error', e);
+		throw e;
+	}
+	employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+	employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+	employeePayrollData.department = getSelectedValues('[name=department]');
+	employeePayrollData.salary = getInputValueById('#salary');
+	employeePayrollData.note = getInputValueById('#notes');
+	let date = getInputValueById('#day')+" "+getInputValueById('#month')+" "+getInputValueById('#year');
+	employeePayrollData.startDate = Date.parse(date);
+	alert(employeePayrollData.toString());
+	return employeePayrollData;
+}
+
+const getSelectedValues = (propertyValue) =>
+{
+	let allItems = document.querySelectorAll(propertyValue);
+	let selItems = [];
+	allItems.forEach(item => 
+		{
+			if(item.checked) selItems.push(item.value);
+		});
+		return selItems;
+}
+
+const getInputValueById = (id) =>
+{
+	let value = document.querySelector(id).value;
+	return value;
+}
+
+const getInputElementValue = (id) =>
+{
+	let value = document.getElementById(id).value;
+	return value;
+}
